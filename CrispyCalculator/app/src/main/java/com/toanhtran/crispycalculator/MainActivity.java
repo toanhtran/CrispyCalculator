596c537cbf60b743bb09ec796d2da4cccc6ca93b
toanhtran <toanh_tran@yahoo.com>
2015-10-05 11:28:50 +0900

    addition btn function

--- CrispyCalculator/app/src/main/java/com/toanhtran/crispycalculator/MainActivity.java
+++ CrispyCalculator/app/src/main/java/com/toanhtran/crispycalculator/MainActivity.java
@@ -5,16 +5,48 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText operand1;//first input field
+    private EditText operand2;//second input field
+    private Button btnAdd;// Addition button that adds numbers
+    private Button btnSub;// Subtraction button that subtracts numbers
+    private Button btnDiv;// Division button that divides numbers
+    private Button btnMulti;//Multiplication button that multiplies numbers
+    private TextView txtResults;//Shows result of calculated numbers
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        operand1 = (EditText) findViewById(R.id.editOperand1);
+        operand2 = (EditText) findViewById(R.id.editOperand2);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+        btnMulti = (Button) findViewById(R.id.btnMulti);
+        txtResults = (TextView) findViewById(R.id.txtResult);
+
+        btnAdd.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                double oper1 = Double.parseDouble(operand1.getText().toString());//coverts number from field 1 to a string
+                double oper2 = Double.parseDouble(operand2.getText().toString());//converts number from field 2 to a string32
+
+                double theResult = oper1 + oper2;//add the # from oper1 to # from oper2
+                txtResults.setText(Double.toString(theResult));//Shows the results and converts value to a string
+
+            }
+
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
